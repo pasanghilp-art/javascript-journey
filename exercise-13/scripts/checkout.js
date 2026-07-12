bad531ee0ad145927531911d3333fd75537218c2
Pasang Tamang <pasanghilp@gmail.com>
2026-07-12 13:42:54 +0545

    making update button interactive, adding two html elements after update & save appear when clicking update

--- exercise-13/scripts/checkout.js
+++ exercise-13/scripts/checkout.js
@@ -35,9 +35,11 @@ cart.forEach((cartItem)=> {
                     <span>
                         Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
-                    <span class="update-quantity-link link-primary">
+                    <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
                         Update
                     </span>
+                    <input class="quantity-input">
+                    <span class="save-quantity-link">Save</span>
                     <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                         Delete
                     </span>
@@ -117,3 +119,15 @@ document.querySelector('.js-return-to-home-link')
 }
 
 updateCartQuantity();
+
+document.querySelectorAll('.js-update-link')
+    .forEach((link)=>{
+        link.addEventListener('click', ()=>{
+            const productId = link.dataset.productId;
+            
+            const container = document.querySelector(
+                `.js-cart-item-container-${productId}`
+            );
+            container.classList.add('is-editing-quantity');
+        });
+    });

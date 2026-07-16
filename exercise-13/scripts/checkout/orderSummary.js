a227dfbd4ef399ce1ba1a76ac49668899d6f8ec6
Pasang Tamang <pasanghilp@gmail.com>
2026-07-16 14:31:58 +0545

    making reusable dayjs and reusing it as function

--- exercise-13/scripts/checkout/orderSummary.js
+++ exercise-13/scripts/checkout/orderSummary.js
@@ -9,7 +9,8 @@ import { getProduct, products } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions,
-        getDeliveryOption
+        getDeliveryOption,
+        calculateDeliveryDate
  } from "../../data/deliveryOptions.js";
  import { renderPaymentSummary } from "./paymentSummary.js";
  import {renderCheckoutHeader} from './checkoutHeader.js';
@@ -26,10 +27,7 @@ export function renderOrderSummary() {
 
         const deliveryOption = getDeliveryOption(deliveryOptionId);
         
-
-        const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-        const dateString = deliveryDate.format("dddd, MMMM D");
+        const dateString = calculateDeliveryDate(deliveryOption);
 
         cartSummaryHTML += `
             <div class="cart-item-container 
@@ -81,9 +79,7 @@ export function renderOrderSummary() {
         let html = "";
 
         deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-            const dateString = deliveryDate.format("dddd, MMMM D");
+            const dateString = calculateDeliveryDate(deliveryOption);
 
             const priceString =
                 deliveryOption.priceCents === 0

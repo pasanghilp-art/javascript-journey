27423ecb970abefcc0b7f8a3099aa9025579e731
Pasang Tamang <pasanghilp@gmail.com>
2026-07-20 13:38:27 +0545

    convert products into classes

--- exercise-13/scripts/checkout.js
+++ exercise-13/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import '../data/cart-oop.js';
+//import '../data/cart-oop.js';
 
 renderCheckoutHeader();
 renderOrderSummary();

6863de60016ec86e61831990ebe6e3582de06216
Pasang Tamang <pasanghilp@gmail.com>
2026-07-21 18:53:08 +0545

    Learn promises and convert code to promises

--- exercise-13/data/products.js
+++ exercise-13/data/products.js
@@ -110,8 +110,7 @@ export function loadProducts(fun){
     }
     return new Product(productDetails);
 });
-
-console.log('load products');
+console.log('load product');
 fun();
   });
   xhr.open('GET', 'https://supersimplebackend.dev/products');
--- exercise-13/scripts/checkout.js
+++ exercise-13/scripts/checkout.js
@@ -2,11 +2,56 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {loadProducts} from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 //import '../data/backend-practice.js';
 //import '../data/cart-oop.js';
 
-loadProducts(()=>{
+
+Promise.all([
+        new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve)=>{
+            loadCart(()=>{
+                resolve();
+            });
+        })
+]).then((values)=>{
+    console.log(values);
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1');
+    });
+
+}).then((value)=>{
+    console.log(value)
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    });
+
+}).then(()=>{
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+*/
+
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderCheckoutHeader();
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/

b74b43a5cea09170b67f2759561b4d01002345fc
Pasang Tamang <pasanghilp@gmail.com>
2026-07-21 17:28:32 +0545

    Add backend practice file

--- exercise-13/scripts/checkout.js
+++ exercise-13/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import '../data/backend-practice.js';
 //import '../data/cart-oop.js';
 
 renderCheckoutHeader();
